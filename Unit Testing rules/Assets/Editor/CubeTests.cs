3265806b3df937532d1d27452b9fe1c046dfb35c
Vasil Kichev <vasilkichev@gmail.com>
2020-12-12 17:02:33 +0100

    Initial Upload

---
+++ Unit Testing rules/Assets/Editor/CubeTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CubeTests
+    {
+        [Test]
+        [TestCase(0)]
+        [TestCase(-29)]
+        public void DisableOnDeath_EmptyHP_ObjectSetInactive(float hp)
+        {
+            GameObject testObject = MakeFakeCube(hp);
+
+            Assert.IsFalse(testObject.activeSelf);
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(100)]
+        [TestCase(0.6f)]
+        [TestCase(9999999)]
+        public void DisableOnDeath_HasHP_ObjectRemainsActive(float hp)
+        {
+            GameObject testObject = MakeFakeCube(hp);
+
+            Assert.IsTrue(testObject.activeSelf);
+        }
+
+        private static GameObject MakeFakeCube(float hp)
+        {
+            GameObject testObject = new GameObject();
+            Cube cubeScript = testObject.AddComponent<Cube>();
+
+            cubeScript.health = hp;
+            cubeScript.DisableOnDeath();
+            return testObject;
+        }
+    }
+}
---
+++ Unit Testing rules/Assets/Scripts/Cube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public float health;
+    void Update()
+    {
+        DisableOnDeath();
+    }
+
+    public void DisableOnDeath()
+    {
+        if (health <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }    
+}
